af1d7705462829d12d6fc0b985797ae565dbfca0
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2015-06-09 16:31:28 -0300

    ad fix

--- src/MultiCliente.java
+++ src/MultiCliente.java
@@ -3,33 +3,34 @@ public class MultiCliente {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(" MULTICLIENTE ON LINE");
+System.out.println(" MULTICLIENTE generando Clientes");
 
   int Tiempo = 15;
-  int NumCliente=46;
+  int NumCliente=40;
   int Puerto=9002;
   String Ip="localhost";
   String Mensage="";
   Cliente c;
   int randomNum =0;
-  
-  for(int i=1;i<=NumCliente;i++){
-	  randomNum = 1 + (int)(Math.random()*100);
+  int i;
+  for(i=1;i<=NumCliente;i++){
+	  randomNum = 1 + (int)(Math.random()*10);
 	 try {
 		Thread.sleep(i*randomNum);
-		  System.out.println(" randomNum: "+randomNum);
+		 // System.out.println(" randomNum: "+randomNum);
 		
 	} catch (InterruptedException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	  System.out.println(" Cliente N°:"+i);
+	//  System.out.println(" Cliente N°:"+i+" creado");
 	  Mensage=" "+i+" 1";
 	  c=new Cliente(Ip, i, Mensage, Puerto, Tiempo);
 	 c.start();
 	
   }
-
+  System.out.print("CLR");
+  System.out.println(" MULTICLIENTE finalizado  con "+i+"clientes");
 
 	}
 
